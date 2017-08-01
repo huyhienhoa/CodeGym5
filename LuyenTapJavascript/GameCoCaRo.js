3d10ad080b01594a54e2f284b2ff8a080e5c3fab
Huy Hien Hoa <huyhienhoacnt@gmail.com>
2017-08-01 23:13:44 +0700

    game cờ caro

--- LuyenTapJavascript/GameCoCaRo.js
+++ LuyenTapJavascript/GameCoCaRo.js
@@ -1,94 +1,114 @@
-function Cell(row,col,flag,image) {
-    this.row=row;
-    this.col=col;
-    this.flag=flag;
-    this.image=image;
-
-    this.hienthi=function (event) {
-             var top=event.clientX-300;
-            var left=event.clientY;
-            return '<img width="'+ 25 + '"' +
-            ' height="'+ 25 + '"' +
-            ' src="' + this.image +'"'+
-        ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-        }
-    document.addEventListener("click",this.hienthi);
-    this.setImage=function (image) {
-        this.image=image;
-    }
-}
-
-function setCell() {
-    var arrCell=[];
-    for(var i=0;i<20;i++){
-        arrCell[i]=new Cell(30*i,300+30*i,0);
-    }
-    return arrCell;
-}
-var buocdi=1;
+var buocdi = 1;
 function myFunction(event) {
-    var x=getMousePos(event);
-    var y=getMousePosY(event);
-    
+    var x = getMousePos(event);
+    var y = getMousePosY(event);
+
 
     var c = document.getElementById("tb").childNodes;
-    if(y>=0 && y<20 && x>=0 && x<20){
-        console.log(y);
-        console.log(x);
-        if(buocdi==1){
-            c[1].childNodes[y*2].childNodes[x*2+1].style.backgroundImage="url('x.PNG')";
-            buocdi=0;
+    if (y >= 0 && y < 20 && x >= 0 && x < 20) {
+        if (buocdi == 1) {
+            c[1].childNodes[y * 2].childNodes[x * 2 + 1].style.backgroundImage = "url('x.PNG')";
+            check(x, y);
+            buocdi = 0;
         }
-        else{
-            c[1].childNodes[y*2].childNodes[x*2+1].style.backgroundImage="url('images/o.PNG')";
-            buocdi=1;
+        else {
+            c[1].childNodes[y * 2].childNodes[x * 2 + 1].style.backgroundImage = "url('images/o.PNG')";
+            check(x, y);
+            buocdi = 1;
         }
 
     }
-    // console.log(c[1].childNodes[2].childNodes[1]);
-    // console.log(c[1].childNodes[4].childNodes[3]);
-}
-
-function check() {
-    myFunction();
-    // document.getElementById('test').innerHTML=x;
 
 }
+
+function checkhang(x, y) {
+    var cot = x;
+    var hang = y;
+    var countRow = 1;
+    var dem = 0;
+    var c = document.getElementById("tb").childNodes;
+    var url = c[1].childNodes[hang * 2].childNodes[cot * 2 + 1].style.backgroundImage;
+    console.log(url);
+    for (var i = cot + 1; i < 20; i++) {
+        if (c[1].childNodes[hang * 2].childNodes[(i + 1) * 2 + 1].style.backgroundImage === url) {
+            countRow++;
+            //cot++;
+        }
+        else {
+            break;
+        }
+
+    }
+    for (var j = cot - 1; j >= 0; j--) {
+        if (c[1].childNodes[hang * 2].childNodes[(j - 1) * 2 + 1].style.backgroundImage === url) {
+            countRow++;
+            //cot++;
+        }
+        else {
+            break;
+        }
+
+    }
+    if (countRow >= 4) {
+        return 1;
+    }
+    return 0;
+
+}
+
+function checkcot(cot, hang) {
+    var countRow = 1;
+    var c = document.getElementById("tb").childNodes;
+    var url = c[1].childNodes[hang * 2].childNodes[cot * 2 + 1].style.backgroundImage;
+    for (var i = hang + 1; i < 20; i++) {
+        if (c[1].childNodes[(i + 1) * 2].childNodes[cot * 2 + 1].style.backgroundImage === url) {
+            countRow++;
+            //cot++;
+        }
+        else {
+            break;
+        }
+
+    }
+    for (var j = hang - 1; j >= 0; j--) {
+        if (c[1].childNodes[(j - 1) * 2].childNodes[cot * 2 + 1].style.backgroundImage === url) {
+            countRow++;
+            //cot++;
+        }
+        else {
+            break;
+        }
+    }
+    if (countRow >= 4) {
+        return 1;
+    }
+    return 0;
+
+}
+
+function check(cot, hang) {
+    var c = document.getElementById("tb").childNodes;
+    if (checkhang(cot, hang) || checkcot(cot, hang)) {
+        if (c[1].childNodes[hang * 2].childNodes[cot * 2 + 1].style.backgroundImage === 'url("x.PNG")') {
+            alert("Người chơi X chiến thắng");
+        }
+        else {
+            alert("Người chơi O chiến thắng");
+        }
+    }
+
+}
+
 function getMousePos(event) {
-        var x=event.clientX;
-        var cot=Math.floor((x-300)/30);
-        return cot;
+    var x = event.clientX;
+    var cot = Math.floor((x - 300) / 30);
+    return cot;
 }
 function getMousePosY(event) {
-    var y=event.clientY;
-    var hang=Math.floor(y/30);
+    var y = event.clientY;
+    var hang = Math.floor(y / 30);
     return hang;
 }
 document.addEventListener("click", getMousePos);
 document.addEventListener("click", getMousePosY);
-document.addEventListener("click",myFunction);
-
-// function test() {
-//     var x=getMousePos(event);
-//     var y=getMousePosY(event);
-//     var arr=setCell();
-//     for(var i=0;i<arr.length;i++){
-//         if(x<arr[i].left+30&&y<arr[i].top+30&&arr[i].flag==0){
-//             if(buocdi==1)
-//             {
-//                 arr[i].flag=1;
-//                 arr[i].image='x.PNG';
-//             }else{
-//                 arr[i].flag=2;
-//                 arr[i].image='o.PNG';
-//             }
-//         }
-//     }
-//
-// }
-
-function start() {
-    buocdi--;
-    test();
-    buocdi++;
-}
+document.addEventListener("click", myFunction);
